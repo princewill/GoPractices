35444682c1af0be33b76f421269efb9de96c27e1
princewill ohuabunwa <wohuabun@gmail.com>
2020-02-25 21:42:34 -0500

    map

--- src/map-range.go
+++ src/map-range.go
@@ -5,5 +5,24 @@ import "fmt"
 func main() {
 
 	a := make(map[string]int)
-	
+
+	a["k1"] = 1
+	a["k2"] = 2
+
+	fmt.Println(a)
+
+	v1 := a["k1"]
+	fmt.Println(v1)
+
+	fmt.Println(len(a))
+
+	delete(a, "k1")
+	fmt.Println(a)
+
+	_, prs := m["k2"]
+	fmt.Println("prs:", prs)
+
+	n := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println("map:", n)
+
 }
