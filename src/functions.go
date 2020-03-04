418ef1245279cc88994f04cf6309275010bf327f
princewill ohuabunwa <wohuabun@gmail.com>
2020-03-03 20:07:35 -0500

    functions

--- src/functions.go
+++ src/functions.go
@@ -1,5 +1,18 @@
 package main
+import "fmt"
 
+func plus(a int, b int) int {
+
+	return a + b
+}
+
+func plusPlus(a, b, c int) int {
+	return a + b + c
+}
 func main() {
-	$END$
+
+		res := plus(1, 2)
+	fmt.Println("1+2 =", res)
+	res = plusPlus(1, 2, 3)
+	fmt.Println("1+2+3 =", res)
 }
